bdad292621d2dbab99e17620621201eac2e219c7
Andrew Nguyen Vo <awnguy01@louisville.edu>
2019-12-02 08:17:34 -0500

    Final iteration

--- cecs_535_project.sql
+++ cecs_535_project.sql
@@ -60,6 +60,7 @@ CREATE TRIGGER update_discount_check
 DELIMITER $$
 CREATE PROCEDURE commission_check_proc(author_id VARCHAR(255), isbn VARCHAR(255), commission FLOAT)
 	BEGIN
+		SET @sum = 0;
 		IF commission < 0 OR commission > 100
 			THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ERROR: Commission not a valid percentage';
 		END IF;
@@ -67,21 +68,12 @@ CREATE PROCEDURE commission_check_proc(author_id VARCHAR(255), isbn VARCHAR(255)
 			INTO @sum
 			FROM Writes 
 			WHERE Writes.isbn = isbn 
-			GROUP BY Writes.isbn
-			LIMIT 1;
-		SELECT Writes.commission 
-			INTO @exclusion
-			FROM Writes 
-			WHERE Writes.author_id = author_id 
-			AND Writes.isbn = isbn
-			LIMIT 1;
+				AND author_id <> Writes.author_id
+			GROUP BY Writes.isbn;
 		IF @sum IS NULL
 			THEN SET @sum = 0;
 		END IF;
-		IF @exclusion IS NULL
-			THEN SET @exclusion = 0; 
-		END IF;
-		IF @sum - @exclusion + commission > 100
+		IF @sum + commission > 100
 			THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ERROR: Sum of commissions over 100%';
 		END IF;
     END;$$
@@ -94,14 +86,11 @@ CREATE TRIGGER update_commission_check
 	BEFORE UPDATE ON Writes
     FOR EACH ROW
     CALL commission_check_proc(NEW.author_id, NEW.isbn, NEW.commission);
--- CREATE TRIGGER delete_commission_check
--- 	BEFORE DELETE ON Writes
---     FOR EACH ROW
---     CALL commission_check_proc(0);
+
 DELIMITER $$
 CREATE PROCEDURE sales_check_proc(number INT)
 	BEGIN
-		IF number < 0
+		IF number <= 0
 			THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ERROR: Invalid number of books sold';
 		END IF;
 	END;$$
@@ -128,8 +117,9 @@ VALUES
 	('0008493847265', 'Secret Heart', 44, 9.99, 2010, 5),
 	('0004827364897', 'Chronicles of Swarnia', 45, 11.99, 2008, 1),
 	('0001923482019', 'Jim Jam Goes the Zim Zam', 44, 4.99, 2018, 1),
-	('0003928401923', 'Light Club', 48, 5.99, 2017, 4),
-	('0004720192841', 'The Tale of Twine', 46, 10.99, 1999, 3);
+	('0003928401923', 'Light Club', 48, 12.99, 2017, 4),
+	('0004720192841', 'The Tale of Twine', 46, 10.99, 1999, 3),
+    ('0008674923846', 'Dark Heart', 43, 11.99, 2012, 5);
 INSERT INTO Author
 	(name, age, address, affiliation)
 VALUES
@@ -138,6 +128,7 @@ VALUES
 	('Marwa Spooner', 41, '79 Dunbar St, Carlisle. PA 17013', 'Writer\'s Guild of Pennsylvania'),
 	('Kasey Goodwin', 33, '8047 Peg Shop Dr. Hanover Park, IL 60133', null),
 	('Eiliyah Ramirez', 25, '506 Bald Hill St, Blackwood, NJ 08012', null),
+    ('Joshua Eichenberg', 28, '444 Georges Ln, Helena, MT 59601', null),
     ('Eva Dixon', 31, '44 Stillwater Rd. Blacksburg, VA 24060', null);
 INSERT INTO Writes
 	(author_id, isbn, commission)
@@ -147,6 +138,7 @@ VALUES
   	(4, '0008493847265', 70),
 	(1, '0001923482019', 100),
  	(2, '0004720192841', 100),
+    (3, '0008674923846', 100),
  	(5, '0003928401923', 100);
 INSERT INTO Sales
 	(isbn, year, month, number)
@@ -160,6 +152,7 @@ VALUES
 	('0003928401923', 2019, 7, 15),
 	('0003928401923', 2019, 8, 13),
 	('0004720192841', 2019, 7, 18),
+    ('0008674923846', 2019, 7, 11),
 	('0004720192841', 2019, 8, 15);
 DELIMITER $$
 CREATE TRIGGER update_inventory
@@ -182,13 +175,52 @@ CREATE TABLE Royalties (
     PRIMARY KEY (author_id),
     FOREIGN KEY (author_id) REFERENCES Author(author_id)
 );
--- INSERT INTO Royalties
--- 	(author_id, amount)
--- VALUES
---     SELECT discount, price, author_id, commission, number
--- 		FROM Publisher, Books, Author, Writes, Sales
---         WHERE Publisher.publisherid = Books.publisherid 
--- 			AND Books.isbn = Writes.isbn
---             AND Writes.author_id = Author.author_id
--- 			AND Sales.isbn = Books.isbn
--- 		GROUP BY author_id
+
+INSERT INTO Royalties
+SELECT author_id, ifnull(SUM(royalties), 0) as amount
+	FROM (SELECT author_id, (price - discount) * commission / 100 * number as royalties
+		FROM (SELECT author_id, v_join_sales.isbn, commission, number, price, publisherid 
+			FROM (SELECT author_id, v_join_writes.isbn, commission, number
+				FROM (SELECT v_trunc_author.author_id as author_id, isbn, commission
+					FROM (SELECT author_id 
+							FROM author) as v_trunc_author
+					LEFT JOIN (SELECT author_id, isbn, commission
+								FROM writes) as v_trunc_writes
+					ON v_trunc_author.author_id = v_trunc_writes.author_id) AS v_join_writes
+				LEFT JOIN (SELECT isbn, SUM(number) as number
+							FROM Sales
+                            GROUP BY isbn) as v_trunc_sales
+				ON v_join_writes.isbn = v_trunc_sales.isbn) AS v_join_sales
+			LEFT JOIN books
+				ON v_join_sales.isbn = books.isbn) AS v_join_books
+		LEFT JOIN (SELECT publisherid, discount
+					FROM publisher) v_trunc_publisher 
+			ON v_join_books.publisherid = v_trunc_publisher.publisherid
+		GROUP BY author_id, isbn) AS v_royalties_by_isbn
+	GROUP BY author_id;
+    
+DELIMITER $$
+CREATE TRIGGER init_royalties_author
+	AFTER INSERT ON Author
+    FOR EACH ROW
+		INSERT INTO Royalties (author_id, amount)
+		VALUE (NEW.author_id, 0);
+$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER update_royalties_sale
+	AFTER INSERT ON Sales
+    FOR EACH ROW
+			UPDATE Royalties
+            INNER JOIN (SELECT author_id, price, discount, commission
+				FROM Writes, 
+					(SELECT isbn, price, publisherid FROM Books) as v_trunc_books,
+					(SELECT publisherid, discount FROM Publisher) as v_trunc_publisher
+				WHERE Writes.isbn = NEW.isbn
+				AND v_trunc_books.isbn = Writes.isbn
+				AND v_trunc_publisher.publisherid = v_trunc_books.publisherid) AS v_join_sales
+			SET Royalties.amount = ((price - discount) * commission / 100 * NEW.number) + Royalties.amount
+            WHERE Royalties.author_id = v_join_sales.author_id;
+$$
+DELIMITER ;
